5d5c4cd515872b9f5642c0f36b62cdccbfca4502
和知舞香 <revolution.w88@gmail.com>
2023-10-21 23:55:40 +0900

    authenticate_user!メソッド追加 - ログインユーザー以外は作成, 編集, 削除が出来ないようにコントローラー実装 - 記事一覧から作成, 編集, 削除のボタンを - 記事詳細の「編集」ボタンは 自分の所有記事である時のみ表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root to: "articles#index"
   devise_for :users
   resources :articles
   resources :sample_articles
-  root to: "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
